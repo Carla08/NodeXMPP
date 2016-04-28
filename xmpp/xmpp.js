adae346d51ebc0477d920773d352948e938c52c4
Mario Contreras <mario.acc.15@gmail.com>
2016-04-28 16:06:28 -0600

    node module

--- xmpp/xmpp.js
+++ xmpp/xmpp.js
@@ -10,44 +10,32 @@ var port = 5222;
 //var logged = false;
 
 //Set for connection 
-var client = new Client({
-	jid: "admin"+domain,
-	password: "password",
-	host: host,
-	port: port
-});
+var client;
 
-
-//Connect AKA LogIn 
-function LogIn(){
-	client.on("online", function(data) {
-		console.log("online");
-		//logged = true;
-		//THIS NEEDS A FIX. it goes here for the asyncronity of Node.js
-		sendMessage("carla@localhost", "sup from Node");
-	});
-
-	//Set presence to available.
-	client.send(new Client.Stanza("presence", {"type":"available"})
-	.c("show").t("chat").up()
-	.c("status").t("Hi I am here stanzas"));
-
-
-}
- 
-
-//Message stanzas
-function sendMessage(to, msg){
-	//while (logged === false) {;}
-	console.log("entered the send message");
-	var stanza = new Client.Stanza("message", {to: to, type: "chat"}).c("body").t(msg);
-	client.send(stanza);
-}
-
-//Test
-function test () {
-	LogIn();
-	//while (logged === false){}
-	
-}
+module.exports ={
+	login: function (user,password) {
+		var client = new Client({
+			jid: user + domain,
+			password: password,
+			host: host,
+			port: port
+		});
+		client.send(new Client.Stanza("presence", {"type": "available"})
+			.c("show").t("chat").up()
+			.c("status").t("Hi I am here stanzas"));
+		client.on("online", function (data) {
+			console.log("online");
+			//logged = true;
+			//THIS NEEDS A FIX. it goes here for the asyncronity of Node.js
+			sendMessage("carla@localhost", "sup from Node");
+		});
+		
+	},
+	sendMessage:function (to, msg){
+		//while (logged === false) {;}
+		console.log("entered the send message");
+		var stanza = new Client.Stanza("message", {to: to, type: "chat"}).c("body").t(msg);
+		client.send(stanza);
+	}
+};
 test();
